3dc60f3cf48a59a118b257b4996c571d9a345d48
elsonigo <jonas_manser@hotmail.com>
2020-05-04 16:20:17 +0200

    svelte template works in ie11

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hoi, {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 3em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
